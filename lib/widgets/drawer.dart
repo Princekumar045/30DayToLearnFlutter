a78476bc1020e11869bb9d92ceb9edc3c5959cf9
Princekumar <princekumar68189@gmail.com>
2025-01-27 18:23:12 +0530

    day5

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = 
+      "https://s.ndtvimg.com/images/entities/300/virat-kohli-967.png";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero ,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                decoration: BoxDecoration(
+                color: Colors.black
+              ),
+                accountName: Text("Virat Kohil"),
+                accountEmail: Text("Viratkohil@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                )
+              )
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.home,
+              color: Colors.white),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.profile_circled,
+              color: Colors.white),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.mail,
+              color: Colors.white),
+              title: Text(
+                "Email Me",
+                textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
